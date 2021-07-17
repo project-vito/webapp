073205779e2cf60a20b8b3c88069d65c6e652a97
Miller Gonzalez <millertaker@geeklancers.com>
2021-07-17 18:37:31 -0500

    list item working

--- src/components/GridItem.js
+++ src/components/GridItem.js
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom';
 import React, { useEffect, useState, useContext } from 'react';
+import Health from './Health';
 
 const GridItem = props => {  
   return (
     <li className="col-6 col-sm-6 col-md-4 grid-item">
-      <Link to="/">
+      <Link to={`/pet-page/${props.id}`}>
         <div className="image-wrapper" style={{backgroundImage: `url(${props.thumb})`}}>
-
+          <Health level={props.sickness} />
         </div>
         <p><b>{props.name}</b></p>
       </Link>
